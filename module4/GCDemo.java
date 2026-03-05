9253c2c04be7ddabb551561ed8fd3f9a5a7f0ec2
gcreddy54 <greddyns@gmail.com>
2026-03-05 09:10:39 +0530

    Java Programmes

---
+++ module4/GCDemo.java
@@ -0,0 +1,15 @@
+package Assignment.module4;
+
+public class GCDemo {
+    public static void main(String[] args) {
+        GCDemo obj1 = new GCDemo();
+        obj1 = null;
+
+        System.gc();
+        System.out.println("Garbage Collection Requested");
+    }
+
+    protected void finalize() {
+        System.out.println("Garbage Collected");
+    }
+}
